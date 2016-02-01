2e860a20b7b6698bb2784b2cf11c88262b435d4b
Heath <Heath.kang@foxmail.com>
2016-02-01 22:11:19 +0800

    添加web表单 Flash消息 例子，包括py文件以及相关的html文件

--- hello4web.py
+++ hello4web.py
@@ -1,7 +1,7 @@
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
-from flask import Flask,render_template,url_for,session,redirect
+from flask import Flask,render_template,url_for,session,redirect,flash
 from flask.ext.script import Manager
 
 from flask.ext.wtf import Form
@@ -22,9 +22,11 @@ moment=Moment(app)
 @app.route('/',methods=['GET','POST'])
 def index():
     img = url_for('static',filename='favicon.ico')
-    name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html',img=img,current_time=datetime.utcnow(),form=form,name=session.get('name'))
