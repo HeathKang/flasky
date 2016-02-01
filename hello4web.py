85385fe21d0fdcfd9f15080e17e661bc6359fd73
Heath <Heath.kang@foxmail.com>
2016-02-01 21:48:37 +0800

    更改web表单程序，使其重定向不必在请求

--- hello4web.py
+++ hello4web.py
@@ -1,7 +1,7 @@
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,session,redirect
 from flask.ext.script import Manager
 
 from flask.ext.wtf import Form
@@ -25,9 +25,9 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html',img=img,current_time=datetime.utcnow(),form=form,name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',img=img,current_time=datetime.utcnow(),form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
