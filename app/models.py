469c15d47706ca4d5abc7270919ef3b39db18397
Heath <Heath.kang@foxmail.com>
2016-03-04 21:19:07 +0800

    11.4.2 在服务器上处理富文本

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask.ext.login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from markdown import markdown
+import bleach
 from datetime import datetime
 
 
@@ -50,27 +52,42 @@ class Role(db.Model):
     		role.default = roles[r][1]
     		db.session.add(role)
     	db.session.commit()
+
+
 class Post(db.Model):
-	__tablename__ = 'posts'
-	id = db.Column(db.Integer,primary_key=True)
-	body = db.Column(db.Text)
-	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
-	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
 
-	@staticmethod
-	def generate_fake(count=100):
-		from random import seed,randint
-		import forgery_py
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed,randint
+        import forgery_py
 
-		seed()
-		user_count = User.query.count()
-		for i in range(count):
-			u = User.query.offset(randint(0,user_count - 1)).first()
-			p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
-					 timestamp=forgery_py.date.date(True),
-					 author=u)
-			db.session.add(p)
-			db.session.commit()
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0,user_count - 1)).first()
+            p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
+                    timestamp=forgery_py.date.date(True),
+                    author=u)
+            db.session.add(p)
+            db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags = ['a','abbr','acronym','b','blockquote','code',
+                        'em','i','li','ol','pre','strong','ul',
+                        'h1','h2','h3','p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value,output_format='html'),
+            tags=allowed_tags,strip=True))
+
+db.event.listen(Post.body,'set',Post.on_changed_body)
+
 
 
 
