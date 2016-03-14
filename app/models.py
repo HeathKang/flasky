e274de46fdd7c943ef7c96c003f816430c851a56
Heath <Heath.kang@foxmail.com>
2016-03-14 21:43:36 +0800

    15.2.2测试web服务完成！期间有很多问题一一解决

--- app/models.py
+++ app/models.py
@@ -329,8 +329,8 @@ class User(UserMixin,db.Model):
             'username':self.username,
             'member_since':self.member_since,
             'last_seen':self.last_seen,
-            'posts':url_for('api.get_user_followed_posts',
-                            id=self.id,_external=True),
+            'posts':url_for('api.get_user_followed_posts',id=self.id,_external=True),
+            'followed_posts':url_for('api.get_user_followed_posts',id=self.id,_external=True),
             'post_count':self.posts.count()
         }
         return json_user
@@ -352,6 +352,7 @@ class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer,primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     disabled = db.Column(db.Boolean)
     author_id = db.Column(db.Integer,db.ForeignKey('users.id')) #外键
@@ -364,6 +365,25 @@ class Comment(db.Model):
             markdown(value,output_format='html'),
             tags=allowed_tags,strip=True))
 
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'post': url_for('api.get_post', id=self.post_id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id,
+                                _external=True),
+        }
+        return json_comment
+  
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)   
 
 db.event.listen(Comment.body, 'set', Comment.on_changed_body)
 
