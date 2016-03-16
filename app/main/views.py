b02193577d6dad30030f0f69ad77a6fb1faafe6a
Heath <Heath.kang@foxmail.com>
2016-03-16 20:58:00 +0800

    16性能完成 有点小迷糊

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,7 @@ from .forms import EditProfileForm,EditProfileAdminForm,PostForm,CommentForm
 from ..import db
 from ..models import User,Role,Permission,Post,Comment
 from ..decorators import admin_required,permission_required
+from flask.ext.sqlalchemy import get_debug_queries
 
 @main.route('/',methods=['GET','POST'])
 def index():
@@ -247,3 +248,14 @@ def server_shutdown():
 		abort(500)
 	shutdown()
 	return 'Shutting down...'
+
+@main.after_app_request
+def after_request(responese):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query:%s\nParameters:%s\nDuration:%fs\nContext:%s\n' %
+                (query.statement,query.parameters,query.duration,query.context))
+    return responese
+
+
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_RECORD_QUERIES = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <347832757@qq.com>'
     MAIL_SERVER = 'smtp.qq.com'
@@ -18,6 +19,7 @@ class Config:
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
+    FLASKY_SLOW_DB_QUERY_TIME = 0.5
 
     @staticmethod
     def init_app(app):
